752005a36215f09ac39d7a5742f8a909420d22d1
michelarusso95 <49483343+michelarusso95@users.noreply.github.com>
2019-04-11 15:14:01 +0200

    MR Libri bean

--- ProgettoFinale/src/main/java/com/etl/model/Libri.java
+++ ProgettoFinale/src/main/java/com/etl/model/Libri.java
@@ -1,12 +1,11 @@
 package com.etl.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table ( name = "LIBRI" )  
 public class Libri {
-
+	private Integer id;
 	private String titolo;
 	private String autore;
 	private String genere;
@@ -16,5 +15,75 @@ public class Libri {
 
 
 	public Libri() {}
+	
+	@Id
+	@GeneratedValue (strategy=GenerationType.SEQUENCE, generator = "libriSequence")
+	
+	@SequenceGenerator (name = "libriSequence", allocationSize = 1, sequenceName = "LIBRISEQ")
+	
+	@Column (name = "ID_LIBRI", unique = true, nullable = false)
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column (name = "TITOLO")
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	@Column (name = "AUTORE")
+	public String getAutore() {
+		return autore;
+	}
+
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+	
+	@Column (name = "GENERE")
+	public String getGenere() {
+		return genere;
+	}
+
+	public void setGenere(String genere) {
+		this.genere = genere;
+	}
+	
+	@Column (name = "CASAEDIT")
+	public String getCasaEdit() {
+		return casaEdit;
+	}
+
+	public void setCasaEdit(String casaEdit) {
+		this.casaEdit = casaEdit;
+	}
+	
+	@Column (name = "COSTO")
+	public Double getCosto() {
+		return costo;
+	}
+
+	public void setCosto(Double costo) {
+		this.costo = costo;
+	}
+	
+	@Column (name = "ANNO")
+	public Integer getAnno() {
+		return anno;
+	}
+
+	public void setAnno(Integer anno) {
+		this.anno = anno;
+	}
+	
+	
 
 }
