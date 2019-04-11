7317ec9e3065e365fef81624128ae1260f74c242
Michela <Michela@192.168.0.35>
2019-04-11 14:33:18 +0200

    MR admindao

--- ProgettoFinale/src/main/java/com/etl/dao/AdminDao.java
+++ ProgettoFinale/src/main/java/com/etl/dao/AdminDao.java
@@ -1,5 +1,11 @@
 package com.etl.dao;
 
-public interface AdminDao {
+import java.io.Serializable;
+import java.util.List;
 
+public interface AdminDao <Admin, Id extends Serializable> {
+	public void update(Admin admin);
+	public Admin findByEmail(String email);
+	public void delete(Admin admin);
+	public List<Admin> findAll();
 }
